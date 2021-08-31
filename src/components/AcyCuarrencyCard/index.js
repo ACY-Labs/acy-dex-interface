55c0b5138728488f68509e098a148f6b0b9163ca
Rick <rsulisthio@gmail.com>
2021-08-31 18:32:16 +0700

    fix: fixed background brigtness and position

--- src/components/AcyCuarrencyCard/index.js
+++ src/components/AcyCuarrencyCard/index.js
@@ -1,4 +1,4 @@
-import { Card, Icon, Input } from 'antd';
+import { Card, Icon, Input, Row, Col } from 'antd';
 import AcyIcon from '@/components/AcyIcon';
 import styles from './index.less';
 const AcyCuarrencyCard = ({
@@ -16,24 +16,37 @@ const AcyCuarrencyCard = ({
     <div {...rest} className={styles.acycuarrencycard}>
       
       <div className={styles.cua_body}>
-        <div>
-          <div className={styles.coin} onClick={onChoseToken}>
-            <AcyIcon width={33} name={icon} />
-            {coin}
-            <AcyIcon name="nabla" />
+        
+        <Row>
+          <Col span={24}> {title && <div className={styles.cua_title}>{title}</div>}</Col>
+        </Row>
+        <Row className={styles.bottomRow}>
+        <Col span={11}>
+            <div>
+            <div className={styles.coin} onClick={onChoseToken}>
+              <AcyIcon width={33} name={icon} />
+              {coin}
+              <AcyIcon name="nabla" />
+            
+            </div>
           
           </div>
-          {title && <div className={styles.cua_title}>{title}</div>}
-        </div>
+          </Col>
+          <Col span={13}>
+            <div className={styles.price}>
+            <ul>
+              <li>
+                <Input placeholder="0.0" value={token} onChange={onChangeToken} />
+              </li>
+              {/* <li className={styles.dollar}> ${dollar}</li> */}
+            </ul>
+          </div>
+          </Col>
+          
+        </Row>
         
-        <div className={styles.price}>
-          <ul>
-            <li>
-              <Input placeholder="0.0" value={token} onChange={onChangeToken} />
-            </li>
-            {/* <li className={styles.dollar}> ${dollar}</li> */}
-          </ul>
-        </div>
+        
+        
       </div>
     </div>
   );
--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -190,8 +190,9 @@ class BasicLayout extends React.PureComponent {
         <Layout
           style={{
             ...this.getLayoutStyle(),
-            minHeight: '100vh'
-            // background: styles.radialBg
+            minHeight: '100vh',
+            // background:  "radial-gradient(circle at 20% 20%, #ea5c1f, #1B1B1C)",
+            
           }}
 
           className={styles.radialBg}
