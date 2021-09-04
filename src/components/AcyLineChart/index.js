83d85033454186577e2a1614e738d9931565f485
黄祖斌 <huang-zb18@mails.tsinghua.edu.cn>
2021-09-04 21:09:47 +0800

    fix: make ACYLineChart work

--- src/components/AcyLineChart/index.js
+++ src/components/AcyLineChart/index.js
@@ -5,6 +5,59 @@ import 'echarts/lib/chart/line';
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title'; // 此处是按需引入
 
+let defaultData = [
+  ['2000-06-05', 116],
+  ['2000-06-06', 129],
+  ['2000-06-07', 135],
+  ['2000-06-08', 86],
+  ['2000-06-09', 73],
+  ['2000-06-10', 85],
+  ['2000-06-11', 73],
+  ['2000-06-12', 68],
+  ['2000-06-13', 92],
+  ['2000-06-14', 130],
+  ['2000-06-15', 245],
+  ['2000-06-16', 139],
+  ['2000-06-17', 115],
+  ['2000-06-18', 111],
+  ['2000-06-19', 309],
+  ['2000-06-20', 206],
+  ['2000-06-21', 137],
+  ['2000-06-22', 1000],
+  ['2000-06-23', 85],
+  ['2000-06-24', 94],
+  ['2000-06-25', 71],
+  ['2000-06-26', 106],
+  ['2000-06-27', 84],
+  ['2000-06-28', 93],
+  ['2000-06-29', 85],
+  ['2000-06-30', 73],
+  ['2000-07-01', 83],
+  ['2000-07-02', 125],
+  ['2000-07-03', 107],
+  ['2000-07-04', 82],
+  ['2000-07-05', 44],
+  ['2000-07-06', 72],
+  ['2000-07-07', 106],
+  ['2000-07-08', 107],
+  ['2000-07-09', 66],
+  ['2000-07-10', 91],
+  ['2000-07-11', 92],
+  ['2000-07-12', 113],
+  ['2000-07-13', 107],
+  ['2000-07-14', 131],
+  ['2000-07-15', 111],
+  ['2000-07-16', 64],
+  ['2000-07-17', 69],
+  ['2000-07-18', 88],
+  ['2000-07-19', 77],
+  ['2000-07-20', 83],
+  ['2000-07-21', 111],
+  ['2000-07-22', 57],
+  ['2000-07-23', 55],
+  ['2000-07-24', 60],
+];
+
 class AcyLineChart extends Component {
   constructor(props) {
     super(props);
@@ -13,8 +66,6 @@ class AcyLineChart extends Component {
       xtitle: this.props.xtitle, // x轴类目名称取参
       data: this.props.data,
     };
-
-    console.log(this.props.data);
   }
   componentDidMount() {}
   // getOption 这个函数主要用于配置 option，包括将数据配置进去
@@ -22,13 +73,15 @@ class AcyLineChart extends Component {
   getOption = () => {
     // 组装数据，返回配置 option
     const { imgType, xtitle, data } = this.state;
-    const dataName = xtitle === 'date' ? '时间' : '名称';
-    const currentData = this.props.backData;
 
-    var dateList = this.props.data.map(function(item) {
+    let chartData = this.props.data;
+    if (!this.props.data) {
+      chartData = defaultData;
+    }
+    var dateList = chartData.map(function(item) {
       return item[0];
     });
-    var valueList = this.props.data.map(function(item) {
+    var valueList = chartData.map(function(item) {
       return item[1];
     });
     let showXAxis = this.props.showXAxis;
@@ -135,7 +188,6 @@ class AcyLineChart extends Component {
     };
     return (
       <ReactEcharts
-        key={this.props.data}
         style={{ height: '100%' }}
         option={this.getOption()}
         notMerge
