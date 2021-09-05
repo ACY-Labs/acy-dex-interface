d6364f9f71e26b7784283e3e8b5bb47533f862b9
Rick <rsulisthio@gmail.com>
2021-09-05 16:05:18 +0700

    fix: market margins

--- src/pages/Market/Index.js
+++ src/pages/Market/Index.js
@@ -98,99 +98,97 @@ export class BasicProfile extends Component {
         // const outsideClickRef = useDetectClickOutside({ onTriggered: this.onSearchBlur });
         const { visible, visibleSearchBar, tabIndex, transactionView} = this.state;
         return (
-            <PageHeaderWrapper>
-                <div className={styles.marketRoot}>
-                    <MarketSearchBar dataSourceCoin={dataSourceCoin} dataSourcePool={dataSourcePool}/>
-                    <div className={styles.chartsMain}>
-                        <div className={styles.chartSectionMain}>
-                              <div className={styles.graphStats}>
-                                  <div className={styles.statName}>TVL</div>
-                                  <div className={styles.statValue}>$2.19b</div>
-                              </div>
-                              <div className={styles.chartWrapper}>
-                                  <AcyLineChart  backData={graphSampleData} showXAxis={true} showGradient={true} lineColor='#e29227' bgColor='#29292c'/>
-                              </div>
-                              
-                        </div>
-                        <div className={styles.chartSectionMain}>
-                                <div className={styles.graphStats}>
-                                    <div className={styles.statName}>VOLUME 24H</div>
-                                    <div className={styles.statValue}>$2.19b</div>
-                                </div>
-                                <div className={styles.chartWrapper}>
-                                    <AcyBarChart backData={graphSampleData}/>
-                                </div>
-                                
-                        </div> 
+            <div className={styles.marketRoot}>
+                <MarketSearchBar dataSourceCoin={dataSourceCoin} dataSourcePool={dataSourcePool}/>
+                <div className={styles.chartsMain}>
+                    <div className={styles.chartSectionMain}>
+                          <div className={styles.graphStats}>
+                              <div className={styles.statName}>TVL</div>
+                              <div className={styles.statValue}>$2.19b</div>
+                          </div>
+                          <div className={styles.chartWrapper}>
+                              <AcyLineChart  backData={graphSampleData} showXAxis={true} showGradient={true} lineColor='#e29227' bgColor='#29292c'/>
+                          </div>
+                          
                     </div>
-                    <Row className={styles.marketOverview} justify="space-around">
-                        <Col span={8} >Volume 24H   <strong>$882.20m</strong> <span className={styles.priceChangeUp}>0.36%</span></Col>
-                        <Col span={8} >Fees 24H <strong>$1.66m </strong>    <span className={styles.priceChangeUp}>0.36%</span></Col>
-                        <Col span={8} >TVL  <strong>$2.90b</strong>  <span className={styles.priceChangeDown}>-0.36%</span></Col>
-                    </Row>
-
-                    <div style={{display:"flex", justifyContent:"space-between", alignItems:"baseline"}}>
-                      <h2>Top Tokens</h2>
-                      <h3>
-                        <Link to='/market/list/token' >
-                          Explore
-                        </Link>
-                      </h3>
-                    </div>
-                    <Table 
-                      dataSource={sortTable(dataSourceCoin, "tvl", true).slice(0, this.state.tokenDisplayNumber + 1)} 
-                      columns={columnsCoin.filter(item => item.visible == true)} 
-                      pagination={false}
-                      style={{
-                        marginBottom: "20px"
-                      }}
-                      footer={() => (
-                        <div className={styles.tableSeeMoreWrapper}>
-                          <a className={styles.tableSeeMore} onClick={this.expandTokenTable}>See More...</a>
-                        </div>
-                      )} 
-                    />
-
-                    <div style={{display:"flex", justifyContent:"space-between", alignItems:"baseline"}}>
-                      <h2>Top Pools</h2>
-                      <h3>
-                        <Link to='/market/list/pool' >
-                          Explore
-                        </Link>
-                      </h3>
-                    </div>
-
-                    <Table 
-                      dataSource={sortTable(dataSourcePool, "tvl", true).slice(0, this.state.poolDisplayNumber + 1)} 
-                      columns={columnsPool.filter(item => item.visible == true)} 
-                      pagination={false}
-                      style={{
-                        marginBottom: "20px"
-                      }}
-                      footer={() => (
-                        <div className={styles.tableSeeMoreWrapper}>
-                          <a className={styles.tableSeeMore} onClick={this.expandPoolTable}>See More...</a>
-                        </div>
-                      )} 
-                    />
-                  
-                    <h2>Transactions</h2>
-                    <Table 
-                      dataSource={sortTable(this.filterTransaction(dataSourceTransaction, transactionView), "time", true).slice(0, this.state.transactionDisplayNumber + 1)} 
-                      columns={transactionHeader(transactionView, this.onClickTransaction).filter(item => item.visible == true)} 
-                      pagination={false}
-                      style={{
-                        marginBottom: "20px"
-                      }}
-                      footer={() => (
-                        <div className={styles.tableSeeMoreWrapper}>
-                          <a className={styles.tableSeeMore} onClick={this.expandTransactionTable}>See More...</a>
-                        </div>
-                      )} 
-                    />
-                
+                    <div className={styles.chartSectionMain}>
+                            <div className={styles.graphStats}>
+                                <div className={styles.statName}>VOLUME 24H</div>
+                                <div className={styles.statValue}>$2.19b</div>
+                            </div>
+                            <div className={styles.chartWrapper}>
+                                <AcyBarChart backData={graphSampleData}/>
+                            </div>
+                            
+                    </div> 
                 </div>
-            </PageHeaderWrapper>
+                <Row className={styles.marketOverview} justify="space-around">
+                    <Col span={8} >Volume 24H   <strong>$882.20m</strong> <span className={styles.priceChangeUp}>0.36%</span></Col>
+                    <Col span={8} >Fees 24H <strong>$1.66m </strong>    <span className={styles.priceChangeUp}>0.36%</span></Col>
+                    <Col span={8} >TVL  <strong>$2.90b</strong>  <span className={styles.priceChangeDown}>-0.36%</span></Col>
+                </Row>
+
+                <div style={{display:"flex", justifyContent:"space-between", alignItems:"baseline"}}>
+                  <h2>Top Tokens</h2>
+                  <h3>
+                    <Link to='/market/list/token' >
+                      Explore
+                    </Link>
+                  </h3>
+                </div>
+                <Table 
+                  dataSource={sortTable(dataSourceCoin, "tvl", true).slice(0, this.state.tokenDisplayNumber + 1)} 
+                  columns={columnsCoin.filter(item => item.visible == true)} 
+                  pagination={false}
+                  style={{
+                    marginBottom: "20px"
+                  }}
+                  footer={() => (
+                    <div className={styles.tableSeeMoreWrapper}>
+                      <a className={styles.tableSeeMore} onClick={this.expandTokenTable}>See More...</a>
+                    </div>
+                  )} 
+                />
+
+                <div style={{display:"flex", justifyContent:"space-between", alignItems:"baseline"}}>
+                  <h2>Top Pools</h2>
+                  <h3>
+                    <Link to='/market/list/pool' >
+                      Explore
+                    </Link>
+                  </h3>
+                </div>
+
+                <Table 
+                  dataSource={sortTable(dataSourcePool, "tvl", true).slice(0, this.state.poolDisplayNumber + 1)} 
+                  columns={columnsPool.filter(item => item.visible == true)} 
+                  pagination={false}
+                  style={{
+                    marginBottom: "20px"
+                  }}
+                  footer={() => (
+                    <div className={styles.tableSeeMoreWrapper}>
+                      <a className={styles.tableSeeMore} onClick={this.expandPoolTable}>See More...</a>
+                    </div>
+                  )} 
+                />
+              
+                <h2>Transactions</h2>
+                <Table 
+                  dataSource={sortTable(this.filterTransaction(dataSourceTransaction, transactionView), "time", true).slice(0, this.state.transactionDisplayNumber + 1)} 
+                  columns={transactionHeader(transactionView, this.onClickTransaction).filter(item => item.visible == true)} 
+                  pagination={false}
+                  style={{
+                    marginBottom: "20px"
+                  }}
+                  footer={() => (
+                    <div className={styles.tableSeeMoreWrapper}>
+                      <a className={styles.tableSeeMore} onClick={this.expandTransactionTable}>See More...</a>
+                    </div>
+                  )} 
+                />
+            
+            </div>
         )
     };
 }
