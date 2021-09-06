ffd67edddd7d1f102714c2bd454333f3855c09ff
Chwizdo <chwizdo@gmail.com>
2021-09-06 17:43:47 +0800

    fix(dao): stake history table modification
    table defaulted to 5 rows
    capitalized table headers

--- src/pages/Dao/StakeHistoryTable.js
+++ src/pages/Dao/StakeHistoryTable.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table } from 'antd';
 import SampleStakeHistoryData, {sampleStakeHistoryColumns} from './SampleStakeHistoryData';
 import styles from './styles.less';
 
 
-const StakeHistoryTable = () => (
-  <Table
-    columns={sampleStakeHistoryColumns}
-    dataSource={SampleStakeHistoryData.sort((a, b) => new Date(b.time) - new Date(a.time))}
-    pagination={false}
-    footer={() => (
-      <div className={styles.tableSeeMoreWrapper}>
-        <a className={styles.tableSeeMore}>See More...</a>
-      </div>
-    )}
-  />
-)
+const StakeHistoryTable = () => {
+  const [stakeDisplayNumber, setStakeDisplayNumber] = useState(5)
+
+  return (
+    <Table
+      columns={sampleStakeHistoryColumns}
+      dataSource={SampleStakeHistoryData.sort((a, b) => new Date(b.time) - new Date(a.time)).slice(0, stakeDisplayNumber)}
+      pagination={false}
+      footer={() => (
+        <div className={styles.tableSeeMoreWrapper}>
+          <a
+            className={styles.tableSeeMore}
+            onClick={() => setStakeDisplayNumber((prevState) => prevState + 5)}
+          >
+            See More...
+          </a>
+        </div>
+      )}
+    />
+  )
+}
 
 export default StakeHistoryTable;
