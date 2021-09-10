d21fc3989a69af82fb458b9812b2b28fbfb0590a
Chwizdo <chwizdo@gmail.com>
2021-09-10 23:56:18 +0800

    feat(farms): design table row with sample data
    populate sample data to each row of the table
    click to expand the row

--- src/pages/Farms/FarmsTable.js
+++ src/pages/Farms/FarmsTable.js
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '@/pages/Farms/Farms.less';
 import farmsTableContent from './FarmsTableContent'
 import FarmsTableRow from '@/pages/Farms/FarmsTableRow';
+import FarmsTableHeader from '@/pages/Farms/FarmsTableHeader';
 
 const FarmsTable = () => {
-  // const [tableContent, setTableContent] = {
-  //
-  // }
+  const [searchInput, setSearchInput] = useState('')
+  const [tableRow, setTableRow] = useState(farmsTableContent.map((row) => {
+    const prevRow = { ...row }
+    prevRow.hidden = true
+    return prevRow
+  }))
+  const onRowClick = (index) => setTableRow((prevState) => {
+    const prevTableRow = [ ...prevState ]
+    prevTableRow[index].hidden = !prevTableRow[index].hidden
+    return prevTableRow
+  })
+
+  useEffect(() => console.log(tableRow), [tableRow])
 
   return (
     <div className={styles.tableContainer}>
-      <div className={styles.tableHeaderContainer}>
-        <div className={styles.tableHeaderTitleContainer}>
-          <div className={styles.tableHeaderTitle}>All Farms</div>
-          <div className={styles.tableHeaderSubtitle}>Stake your LP tokens and earn token rewards</div>
-        </div>
-        <div className={styles.tableHeaderButtonContainer}>
-          <div className={styles.tableHeaderRadioButtonContainer}>Stake Only</div>
-          <div className={styles.tableHeaderSearchInputContainer}>Search by Token Symbol</div>
-          <div className={styles.tableHeaderToggleButtonContainer}>Active & Ended</div>
-        </div>
-      </div>
+      <FarmsTableHeader
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+      />
       <div className={styles.tableBodyContainer}>
-        {farmsTableContent.map(() => (
-          <FarmsTableRow />
+        {tableRow.map((content, index) => (
+          <FarmsTableRow
+            key={index}
+            token1={content.token1}
+            token1Logo={content.token1Logo}
+            token1Reward={content.token1Reward}
+            token2={content.token2}
+            token2Logo={content.token2Logo}
+            token2Reward={content.token2Reward}
+            totalApr={content.totalApr}
+            tvl={content.tvl}
+            hidden={content.hidden}
+            rowClickHandler={() => onRowClick(index)}
+          />
         ))}
       </div>
     </div>
--- src/pages/Farms/FarmsTableRow.js
+++ src/pages/Farms/FarmsTableRow.js
@@ -1,14 +1,74 @@
 import React from 'react';
 import styles from '@/pages/Farms/Farms.less';
 
-const FarmsTableRow = () => {
+const FarmsTableRow = (
+  { token1, token1Logo, token1Reward, token2, token2Logo, token2Reward, totalApr, tvl, hidden, rowClickHandler }
+) => {
   return (
     <div className={styles.tableBodyRowContainer}>
-      <div className={styles.tableBodyRowTitleContainer}>Token Pair Name</div>
-      <div className={styles.tableBodyRowRewardContainer}>Pending Reward</div>
-      <div className={styles.tableBodyRowAprContainer}>Total APR</div>
-      <div className={styles.tableBodyRowTvlContainer}>TVL</div>
-      <div className={styles.tableBodyRowArrowContainer}>Arrow Icon</div>
+
+      {/* Table Content */}
+
+      <div className={styles.tableBodyRowContentContainer} onClick={rowClickHandler}>
+
+        {/* Token Title Row */}
+
+        <div className={styles.tableBodyTitleColContainer}>
+          <div className={styles.token1LogoContainer}>
+            <img src={token1Logo} alt={token1} />
+          </div>
+          <div className={styles.token2LogoContainer}>
+            <img src={token2Logo} alt={token2} />
+          </div>
+          <div className={styles.tokenTitleContainer}>
+            {`${token1}-${token2} LP`}
+          </div>
+        </div>
+
+        {/* Pending Reward Column */}
+
+        <div className={styles.tableBodyRewardColContainer}>
+          <div className={styles.pendingRewardTitleContainer}>Pending Reward</div>
+          {token1Reward !== null && (
+            <div className={styles.pendingReward1ContentContainer}>
+              {`${token1Reward} ${token1}`}
+            </div>
+          )}
+          {token2Reward !== null && (
+            <div className={styles.pendingReward2ContentContainer}>
+              {`${token2Reward} ${token2}`}
+            </div>
+          )}
+        </div>
+
+        {/* Total APR Column */}
+
+        <div className={styles.tableBodyAprColContainer}>
+          <div className={styles.totalAprTitleContainer}>Total APR</div>
+          <div className={styles.totalAprContentContainer}>{totalApr}%</div>
+        </div>
+
+        {/* TVL Column */}
+
+        <div className={styles.tableBodyTvlColContainer}>
+          <div className={styles.tvlTitleContainer}>TVL</div>
+          <div className={styles.tvlContentContainer}>${tvl}</div>
+        </div>
+
+        {/* Arrow Icon Column */}
+
+        <div className={styles.tableBodyArrowColContainer}>
+          <svg xmlns="http://www.w3.org/2000/svg" className={styles.arrowIcon} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+          </svg>
+        </div>
+      </div>
+
+      {/* Table Drawer */}
+
+      <div className={styles.tableBodyDrawerContainer} hidden={hidden}>
+        Hello
+      </div>
     </div>
   )
 }
