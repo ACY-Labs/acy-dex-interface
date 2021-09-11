77f3bd60512207906af96c9ea19c8d8ab9f1af9d
Chwizdo <chwizdo@gmail.com>
2021-09-11 13:29:33 +0800

    feat(farms): added expandable drawer for each table row
    changed background color
    changed toggle buttons
    added toggle table contents functionality

--- src/pages/Farms/Farms.js
+++ src/pages/Farms/Farms.js
@@ -1,25 +1,77 @@
 import React, { useState } from 'react';
-import { AcySmallButtonGroup } from '@/components/AcySmallButton';
 import styles from './Farms.less'
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 import FarmsTable from './FarmsTable';
+import farmsTableContent from '@/pages/Farms/FarmsTableContent';
 
 const Farms = () => {
+  const INITIAL_TABLE_DATA = farmsTableContent.map((row) => {
+    const prevRow = { ...row }
+    prevRow.hidden = true
+    return prevRow
+  })
+
   const [selectedTable, setSelectedTable] = useState(0)
+  const [tableRow, setTableRow] = useState(INITIAL_TABLE_DATA)
+
+  const onRowClick = (index) => setTableRow((prevState) => {
+    const prevTableRow = [ ...prevState ]
+    prevTableRow[index].hidden = !prevTableRow[index].hidden
+    return prevTableRow
+  })
+
+  const onAllToggleButtonClick = () => {
+    setSelectedTable(0)
+    setTableRow(INITIAL_TABLE_DATA)
+  }
+
+  const onAcyToggleButtonClick = () => {
+    setSelectedTable(1)
+    setTableRow(INITIAL_TABLE_DATA.filter((tableData) => (
+      tableData.pendingReward.length === 1 && tableData.pendingReward[0].token) === 'ACY'
+    ))
+  }
+
+  const onPremierToggleButtonClick = () => {
+    setSelectedTable(2)
+    setTableRow(INITIAL_TABLE_DATA.filter((tableData) => (
+      (tableData.pendingReward.length === 1 && tableData.pendingReward[0].token) !== 'ACY') || tableData.pendingReward.length !== 1
+    ))
+  }
 
   return (
     <PageHeaderWrapper>
       <div className={styles.farmsContainer}>
-        <AcySmallButtonGroup
-          activeButton={selectedTable}
-          buttonList={[
-            ['All', () => setSelectedTable(0)],
-            ['Radium', () => setSelectedTable(1)],
-            ['Fusion', () => setSelectedTable(2)],
-          ]}
-          containerClass={styles.tableToggleButtonContainer}
+        <div className={styles.tableToggleButtonContainer}>
+          <button
+            type="button"
+            className={styles.firstToggleButton}
+            style={{ borderColor: selectedTable === 0 && '#1d5e91' }}
+            onClick={onAllToggleButtonClick}
+          >
+            All
+          </button>
+          <button
+            type="button"
+            className={styles.middleToggleButton}
+            style={{ borderColor: selectedTable === 1 && '#1d5e91' }}
+            onClick={onAcyToggleButtonClick}
+          >
+            ACY
+          </button>
+          <button
+            type="button"
+            className={styles.lastToggleButton}
+            style={{ borderColor: selectedTable === 2 && '#1d5e91' }}
+            onClick={onPremierToggleButtonClick}
+          >
+            Premier
+          </button>
+        </div>
+        <FarmsTable
+          tableRow={tableRow}
+          onRowClick={onRowClick}
         />
-        <FarmsTable />
       </div>
     </PageHeaderWrapper>
   )
--- src/pages/Farms/FarmsTable.js
+++ src/pages/Farms/FarmsTable.js
@@ -4,20 +4,8 @@ import farmsTableContent from './FarmsTableContent'
 import FarmsTableRow from '@/pages/Farms/FarmsTableRow';
 import FarmsTableHeader from '@/pages/Farms/FarmsTableHeader';
 
-const FarmsTable = () => {
+const FarmsTable = ({ tableRow, onRowClick }) => {
   const [searchInput, setSearchInput] = useState('')
-  const [tableRow, setTableRow] = useState(farmsTableContent.map((row) => {
-    const prevRow = { ...row }
-    prevRow.hidden = true
-    return prevRow
-  }))
-  const onRowClick = (index) => setTableRow((prevState) => {
-    const prevTableRow = [ ...prevState ]
-    prevTableRow[index].hidden = !prevTableRow[index].hidden
-    return prevTableRow
-  })
-
-  useEffect(() => console.log(tableRow), [tableRow])
 
   return (
     <div className={styles.tableContainer}>
@@ -31,14 +19,13 @@ const FarmsTable = () => {
             key={index}
             token1={content.token1}
             token1Logo={content.token1Logo}
-            token1Reward={content.token1Reward}
             token2={content.token2}
             token2Logo={content.token2Logo}
-            token2Reward={content.token2Reward}
             totalApr={content.totalApr}
             tvl={content.tvl}
             hidden={content.hidden}
             rowClickHandler={() => onRowClick(index)}
+            pendingReward={content.pendingReward}
           />
         ))}
       </div>
