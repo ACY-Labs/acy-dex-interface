4887fab320d7b83e1257ad9a36acb35c86c7abed
黄祖斌 <huang-zb18@mails.tsinghua.edu.cn>
2021-09-25 00:25:16 +0800

    feat: get pool info

--- src/acy-dex-swap/core/farms.js
+++ src/acy-dex-swap/core/farms.js
@@ -1,14 +1,36 @@
 import { getFarmsContract } from '@/acy-dex-swap/utils';
 
 const getAllPools = async (library, account) => {
-  const contract = getFarmsContract(library, account)
-  const numPoolHex = await contract.numPools()
-  const numPool = numPoolHex.toNumber()
-  const pools = []
+  const contract = getFarmsContract(library, account);
+  const numPoolHex = await contract.numPools();
+  const numPool = numPoolHex.toNumber();
+  const poolInfoRequests = [];
+  const parsedPoolsInfo = [];
 
   for (let i = 0; i < numPool; i++) {
-    await contract.poolInfo(i).address
-  }
-}
+    poolInfoRequests.push(
+      (async () => {
+        let poolInfo = await contract.poolInfo(i);
+        let rewardTokens = await contract.getPoolRewardTokens(i);
+        let rewardTokensAddresses = await contract.getPoolRewardTokenAddresses(i);
 
-export { getAllPools }
+        console.log(poolInfo);
+
+        return {
+          lpTokenAddress: poolInfo[0],
+          lockDuration: poolInfo[1].toNumber(),
+          lpBalance: poolInfo[2],
+          lastUpdateBlock: poolInfo[3].toNumber(),
+          rewardTokens,
+          rewardTokensAddresses,
+        };
+      })()
+    );
+  }
+
+  Promise.all(poolInfoRequests).then(res => {
+    return res;
+  });
+};
+
+export { getAllPools };
