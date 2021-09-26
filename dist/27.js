c1499e51862cabf6f17f16fa32fbccb6fbcb2c4c
Rick <rsulisthio@gmail.com>
2021-09-26 23:43:17 +0700

    build: market build

--- dist/27.js
+++ dist/27.js
@@ -1,1 +1,1 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[27],{16:function(n,w){}}]);
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[27],{5:function(n,o){},6:function(n,o){}}]);
