f5a64a4a661b7513e3ee2ea797c350e96c3f5097
nearrainbow <near_rainbow@hotmail.com>
2021-11-02 18:45:12 +0700

    launchpad, add swap component

--- src/pages/LaunchPad/swapTicket.js
+++ src/pages/LaunchPad/swapTicket.js
@@ -1,10 +1,55 @@
 import React, { useState, useEffect } from "react";
-import {AcyCuarrencyCard} from '@/components/Acy';
+import SwapInput from './SwapInput';
+import styles from './styles.less';
+import { Card, Icon, Input, Row, Col } from 'antd';
+import AcyIcon from '@/assets/icon_double_down.svg';
+import AcyButton from '@/components/AcyButton';
+
+
+const TICKETPRICE_USDT = 100;
+const TICKETPRICE_ACY  = 1.2;
 
 const swapTicket = (props) => {
-    return (
-      <h1>hi</h1>
-    )
+  const [light, setLight] = useState(false);
+  const onBlur = () => {
+    setLight(false);
+  };
+  const onFocus = () => {
+    setLight(true);
+    inputRef.current.focus();
+  };
+  const inputRef = React.createRef();
+  const [value, setValue] = useState();
+  return (
+    <div>
+      <div className={styles.swapContainer}>
+        <SwapInput
+          title={"Ticket"}
+          onChangeToken={ amount => {
+            setValue(amount)
+          }}
+          value={value}
+          TicketPrice={TICKETPRICE_ACY}
+        />
+        <div className={styles.double_arrow}>
+          <img src={AcyIcon} />
+        </div>
+        <SwapInput
+          title={"USDT"}
+
+          value={value? value * TICKETPRICE_USDT: ''}
+          token={'test'}
+        />
+        <div className={styles.swap_button}
+          onClick={() => {
+            console.log('Participate clicked!')
+          }}
+        >
+          Participate
+        </div>
+      </div>
+  </div>
+  )
 }
 
 export default swapTicket;
