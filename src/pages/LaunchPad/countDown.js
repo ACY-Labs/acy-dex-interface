0e0a10beb881cb8bf0385661cd313c08639bb63e
austin0072009 <2018080084@secoder.net>
2021-11-10 14:15:55 +0800

    Countdown funtion added

--- src/pages/LaunchPad/countDown.js
+++ src/pages/LaunchPad/countDown.js
@@ -7,44 +7,82 @@ import AcyButton from '@/components/AcyButton';
 import { AcyLineChart } from '@/components/Acy';
 
 const { Countdown } = Statistic;
-const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30; // Moment is also OK
 
-function onFinish() {
-    console.log('finished!');
-  }
-  
-  function onChange(val) {
-    if (4.95 * 1000 < val && val < 5 * 1000) {
-      console.log('changed!');
-    }
+  function getRemainTime(now) {
+    var ddl = '2022/11/11 19:22:21';
+    //var now = Date.now()
+    var date1 = new Date(now);
+    var date2 = new Date(ddl);
+
+      var s1 = date1.getTime(),s2 = date2.getTime();
+      var total = (s2 - s1)/1000;
+
+      var day = parseInt(total / (24*60*60));//计算整数天数
+      var afterDay = total - day*24*60*60;//取得算出天数后剩余的秒数
+      var hour = parseInt(afterDay/(60*60));//计算整数小时数
+      var afterHour = total - day*24*60*60 - hour*60*60;//取得算出小时数后剩余的秒数
+      var min = parseInt(afterHour/60);//计算整数分
+      var afterMin = total - day*24*60*60 - hour*60*60 - min*60;//取得算出分后剩余的秒数
+      var sec = parseInt(afterMin);
+
+      return {day:day,hour:hour,min:min,sec:sec};
+
   }
 
   const countDown = (props) => {
 
+    let [currentTime,setTime] = useState(Date.now());
 
+    let [countTime,setCount] = useState({days:0,hours:0,min:0,sec:0});
+
+    let  {day,hour,min,sec } =   getRemainTime(currentTime);
+
+
+    //figure out remain time
+
+    useEffect(async () => {
+
+      let  {day,hour,min,sec } =   getRemainTime(currentTime);
+
+      setCount({days:day,hours:hour,min:min,sec:sec});
+    },[currentTime])
+
+
+    // 实时获取当前时间
+    useEffect(async () => {
+      let timer = setInterval(() => {
+        setTime(Date.now());
+      }, 1000);
+
+      return () => {clearInterval(timer);
+      }
+
+    },[]);
+   
     return(
+      
       <div className={styles.countDown}>
       <div className={styles.cntdwn}>
         <div className = {styles.number} id = "b">
-        <div classNmae = {styles.countnumber}>0</div>
+        <div className = {styles.countnumber} >{countTime.days}</div>
         <div>DAYS</div>
         </div>
         <div className = {styles.seperator}>:</div>
         <div className = {styles.number}id = "b">
 
-        <div classNmae = {styles.countnumber}>0</div>
+        <div className = {styles.countnumber} >{countTime.hours}</div>
         <div>HOURS</div>
         </div>
         <div className = {styles.seperator}>:</div>
         <div  className = {styles.number}id = "b">
 
-        <div classNmae = {styles.countnumber}>0</div>
+        <div className = {styles.countnumber} >{countTime.min}</div>
         <div>MIN</div>
         </div>
         <div className = {styles.seperator}>:</div>
         <div className = {styles.number}id = "b">
 
-        <div classNmae = {styles.countnumber} >0</div>
+        <div className = {styles.countnumber} >{countTime.sec}</div>
         <div>SEC</div>
         </div>
 
