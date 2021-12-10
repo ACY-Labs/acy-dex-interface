81528fe22c1e6c5d863f351ea960d552fb390e0e
Carlos <carlos.ramos@grupoimportia.com>
2021-12-10 11:44:48 +0800

    (poolChart): pool chart done // need to add service to remote API

--- src/pages/Market/Data/poolData.js
+++ src/pages/Market/Data/poolData.js
@@ -98,6 +98,7 @@ export async function fetchGeneralPoolInfoDay() {
     let data = await response.json();
     return parsePoolData(data.data);
   }catch (e){
+    return [];
     console.log('service not available yet');
   }
 }
