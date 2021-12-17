ff82fbd7b58e2f8dfcec2e0d8ea1b753d44bae06
Carlos <carlos.ramos@grupoimportia.com>
2021-12-17 13:21:15 +0800

    merge conflict

---
+++ src/constants/uniqueTokens.js
@@ -0,0 +1,355 @@
+// verify at etherscan with optimization on and compiler version v0.6.7
+export default [
+    {
+      name: 'Ether',
+      symbol: 'ETH',
+      address: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+      addressOnEth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/ETH.svg',
+    },
+    {
+      name: 'USD Coin',
+      symbol: 'USDC',
+      address: '0xA6983722023c67Ff6938FF2adc1d7fC61B5966f3',
+      addressOnEth: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      decimals: 6,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/USDC.svg',
+    },
+    // {
+    //   name: 'Wrapped Ether',
+    //   symbol: 'WETH',
+    //   address: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+    //   addressOnEth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    //   decimals: 18,
+    //   logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/ETH.svg',
+    // },
+    {
+      name: 'ACY',
+      symbol: 'ACY',
+      address: '0xd0da907ec2F98E23ecf75CA12b6C83744dFBDf30',
+      addressOnEth: '0xE41d2489571d322189246DaFA5ebDe1F4699F498', // now this is ZRX addr
+      decimals: 18,
+      logoURI: 'https://acy.finance/static/media/logo.78c0179c.svg',
+    },
+  
+    {
+      name: 'Uniswap',
+      symbol: 'UNI',
+      address: '0xd52F10543673AC7EF890FA94417e665614E7c5A0',
+      addressOnEth: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/UNI.svg',
+    },
+    {
+      name: 'Dai Stablecoin',
+      symbol: 'DAI',
+      address: '0x95d4aA0324aABbF595F0C01F786c3bAB2A6a1b5e',
+      addressOnEth: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/DAI.svg',
+    },
+    {
+      name: 'Compound Dai',
+      symbol: 'cDAI',
+      address: '0x158653b66fd72555F68eDf983736781E471639Cc',
+      addressOnEth: '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643',
+      decimals: 8,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/DAI.svg',
+    },
+    {
+      name: 'Wrapped BTC',
+      symbol: 'WBTC',
+      address: '0x36cB658b45C15B4d5D1C022339ad9931eAE1B850',
+      addressOnEth: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+      decimals: 8,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/WBTC.svg',
+    },
+    {
+      name: 'Aave',
+      symbol: 'AAVE',
+      address: '0x525BF73b0EB085FAaFA64CDb9e8a966B12F91fd7',
+      addressOnEth: '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/AAVE.svg',
+    },
+    // token not exist in testnet
+    // {
+    //   "chainId": 1,
+    //   "address": "0xfF20817765cB7f73d4bde2e66e067E58D11095C2",
+    //   "name": "Amp",
+    //   "symbol": "AMP",
+    //   "decimalss": 18,
+    //   "logoURI": "https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/AMP.svg"
+    // },
+    {
+      name: 'Aragon Network Token',
+      symbol: 'ANT',
+      address: '0xbc1B9d476875C67cabd38F3C7CEdD40144909255',
+      addressOnEth: '0x960b236A07cf122663c4303350609A66A7B288C0',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/ANT.svg',
+    },
+    // balancer token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'Balancer',
+      symbol: 'BAL',
+      address: '0x1740186c527d04ef73913ff9dee6467C3e8A37Cd',
+      addressOnEth: '0xba100000625a3754423978a60c9317c58a424e3D',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/BAL.svg',
+    },
+    // band protocol token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'Band Protocol',
+      symbol: 'BAND',
+      address: '0xE971616d94335dec2E54939118AdFcB68E6dCAd6',
+      addressOnEth: '0xBA11D00c5f74255f56a5E366F4F77f5A186d7f55',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/BAND.svg',
+    },
+    // bancor network token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'Bancor Network Token',
+      symbol: 'BNT',
+      address: '0x5cdbBA00D8b2a54FCB3328aBdb90F69E41ba826f',
+      addressOnEth: '0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/BNT.svg',
+    },
+    // compound token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'Compound',
+      symbol: 'COMP',
+      address: '0x9036d746d3C3AFA8D6D948c071683F68eB53bCb9',
+      addressOnEth: '0xc00e94Cb662C3520282E6f5717214004A7f26888',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/COMP.svg',
+    },
+    {
+      name: 'Curve DAO Token',
+      symbol: 'CRV',
+      address: '0x5b8533c9a6C330EE1E376dba1AFe7b5e137a76a9',
+      addressOnEth: '0xD533a949740bb3306d119CC777fa900bA034cd52',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/CRV.svg',
+    },
+    {
+      name: 'Civic',
+      symbol: 'CVC',
+      address: '0xfe01074Ec94B62682eBbdcFd5808cEA1cbe6410d',
+      addressOnEth: '0x41e5560054824eA6B0732E656E3Ad64E20e94E45',
+      decimals: 8,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/CVC.svg',
+    },
+    {
+      name: 'district0x',
+      symbol: 'DNT',
+      address: '0x6E59735D808E49D050D0CB21b0c9549D379BBB39',
+      addressOnEth: '0x0AbdAce70D3790235af448C88547603b945604ea',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/DNT.svg',
+    },
+    // gnosis token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'Gnosis Token',
+      symbol: 'GNO',
+      address: '0x61Bb0f893e9DD3C282B80Fd121103258EfB4144d',
+      addressOnEth: '0x6810e776880C02933D47DB1b9fc05908e5386b96',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/GNO.svg',
+    },
+    // the graph token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'The Graph',
+      symbol: 'GRT',
+      address: '0xDadED6F2AcDb532dA15D2815E517f5DdeA5Eaaf1',
+      addressOnEth: '0xc944E90C64B2c07662A292be6244BDf05Cda44a7',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/GRT.svg',
+    },
+    // keep network token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'Keep Network',
+      symbol: 'KEEP',
+      address: '0xeBeD1f4576ADbb81f101bDC332EffD88ff99fCf7',
+      addressOnEth: '0x85Eee30c52B0b379b046Fb0F85F4f3Dc3009aFEC',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/KEEP.svg',
+    },
+    // kyber network crystal token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'Kyber Network Crystal',
+      symbol: 'KNC',
+      address: '0x11e8A51a8Cd00F40e26E0B2785dfAD75aB29aca9',
+      addressOnEth: '0xdd974D5C2e2928deA5F71b9825b8b646686BD200',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/KNC.svg',
+    },
+    {
+      name: 'ChainLink Token',
+      symbol: 'LINK',
+      address: '0x3D2c61C2Dd42f49FAE224657E1367A7c631E319a',
+      addressOnEth: '0x514910771AF9Ca656af840dff83E8264EcF986CA',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/LINK.svg',
+    },
+    {
+      name: 'Loom Network',
+      symbol: 'LOOM',
+      address: '0x0c93Ee0015113e542d29c00bb7E2BB7249533Eb6',
+      addressOnEth: '0xA4e8C3Ec456107eA67d3075bF9e3DF3A75823DB0',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/LOOM.svg',
+    },
+    {
+      name: 'LoopringCoin V2',
+      symbol: 'LRC',
+      address: '0x29304827874DFa721bFA50E65D063D4E7C8Ef559',
+      addressOnEth: '0xBBbbCA6A901c926F240b89EacB641d8Aec7AEafD',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/LRC.svg',
+    },
+    {
+      name: 'Decentraland',
+      symbol: 'MANA',
+      address: '0xEA90E4235113c74B0AFE2ECb4D734E09f1F134Dc',
+      addressOnEth: '0x0F5D2fB29fb7d3CFeE444a200298f468908cC942',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/MANA.svg',
+    },
+    {
+      name: 'Melon',
+      symbol: 'MLN',
+      address: '0xd6937E1Ff83e05d33ABe0af31FbDB2E26217b56c',
+      addressOnEth: '0xec67005c4E498Ec7f55E092bd1d35cbC47C91892',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/MLN.svg',
+    },
+    // numeraire token address in testnet is not verified
+    // address is chosen based on highest transactions
+    // ADDRESS BELOW IS A DUPLICATE OF ADDRESS ABOVE
+    // {
+    //   name: 'Numeraire',
+    //   symbol: 'NMR',
+    //   address: '0xf8236365b3db2534B44C5989aEB49ddB95570f2a',
+    //   addressOnEth: '0xec67005c4E498Ec7f55E092bd1d35cbC47C91892',
+    //   decimals: 18,
+    //   logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/NMR.svg',
+    // },
+    // nucypher token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'NuCypher',
+      symbol: 'NU',
+      address: '0xCd559E6CDF3Aa1690343D64e3aa559C0829046AE',
+      addressOnEth: '0x4fE83213D56308330EC302a8BD641f1d0113A4Cc',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/NU.svg',
+    },
+    // orchid token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'Orchid',
+      symbol: 'OXT',
+      address: '0xc13D02F8134F4AeA81e9434DA0cBf37D101FC58E',
+      addressOnEth: '0x4575f41308EC1483f3d399aa9a2826d74Da13Deb',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/OXT.svg',
+    },
+    // republic token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'Republic Token',
+      symbol: 'REN',
+      address: '0x164028f557339024d937687B5dFd2Fc65a47c48d',
+      addressOnEth: '0x408e41876cCCDC0F92210600ef50372656052a38',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/REN.svg',
+    },
+    {
+      name: 'Reputation Augur v1',
+      symbol: 'REP',
+      address: '0xe7A3a5d135637569FAC74b1CbdF5Ae7f672Baf11',
+      addressOnEth: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/REP.svg',
+    },
+    {
+      name: 'Reputation Augur v2',
+      symbol: 'REPv2',
+      address: '0x11D515a6cC81184D4b6532e4f2d3834D4bBF4360',
+      addressOnEth: '0x221657776846890989a759BA2973e427DfF5C9bB',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/REPv2.svg',
+    },
+    {
+      name: 'Synthetix Network Token',
+      symbol: 'SNX',
+      address: '0x0635937fe3eFd430235fFd985Fa68f64DC17920A',
+      addressOnEth: '0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/SNX.svg',
+    },
+    {
+      name: 'Storj Token',
+      symbol: 'STORJ',
+      address: '0xff1c9fb592b5d61fC92034888d0Dbc540B76D42A',
+      addressOnEth: '0xB64ef51C888972c908CFacf59B47C1AfBC0Ab8aC',
+      decimals: 8,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/STORJ.svg',
+    },
+    {
+      name: 'tBTC',
+      symbol: 'TBTC',
+      address: '0x3Fb7417C73d4A4Dd96E3D526A66D95C47a5727Af',
+      addressOnEth: '0x8dAEBADE922dF735c38C80C7eBD708Af50815fAa',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/TBTC.png',
+    },
+    // republic token address in testnet is not verified
+    // address is chosen based on highest transactions
+    {
+      name: 'UMA Voting Token v1',
+      symbol: 'UMA',
+      address: '0x986BEaaA0252A5fa3E2eD345F1b672B87d8E52a2',
+      addressOnEth: '0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/UMA.svg',
+    },
+    {
+      name: 'Tether USD',
+      symbol: 'USDT',
+      address: '0xC845f2dE2c6ffa6B5689B4348F20763Bd7848af8',
+      addressOnEth: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+      decimals: 6,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/USDT.svg',
+    },
+    {
+      name: 'yearn finance',
+      symbol: 'YFI',
+      address: '0x5262688cfB1A8D884D23202612F582B34ee173d5',
+      addressOnEth: '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
+      decimals: 18,
+      logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/YFI.svg',
+    },
+    // zrx token address in testnet is not verified
+    // address is chosen based on highest transactions
+    // {
+    //   name: '0x Protocol Token',
+    //   symbol: 'ZRX',
+    //   address: '0xd215094219B05aA1d2faE7e7829D1d2edb4346ED',
+    //   addressOnEth: '0xE41d2489571d322189246DaFA5ebDe1F4699F498',
+    //   decimals: 18,
+    //   logoURI: 'https://storageapi.fleek.co/chwizdo-team-bucket/ACY Token List/ZRX.svg',
+    // },
+  ];
+  
