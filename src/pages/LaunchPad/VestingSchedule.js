599b3a339b519f6df23b0afe5564f802ff4014a8
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-12-19 20:20:50 +0800

    feat: vesting schedule component

--- src/pages/LaunchPad/VestingSchedule.js
+++ src/pages/LaunchPad/VestingSchedule.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './css/LaunchpadProject.css';
-import tick from "@/assets/tick-launchpad.svg"
+import tick from '@/assets/tick-launchpad.svg';
 
 const VestingSchedule = () => {
   return (
     <div>
-      <p className='vesting-schedule-title'>Vesting Schedule</p>
       <div className="procedure vesting-procedure">
         <hr aria-orientation="vertical" className="verticalDivideLine vesting-schedule-line" />
         <div className="procedureNumber">
           <img src={tick} alt="tick-icon" />
         </div>
-        <div className='vesting-schedule-text'>
-          <p className='vesting-percentage'>30%</p>
-          <p className="vesting-text"><span className='claimable-text'>Claimable at</span> 12/17/2021, 9:30:00pm</p>
+        <div className="vesting-schedule-text">
+          <p className="vesting-percentage">30%</p>
+          <p className="vesting-text">
+            <span className="claimable-text">Claimable at</span>
+            <br />
+            12/17/2021, 9:30:00pm
+          </p>
         </div>
       </div>
 
@@ -22,9 +25,12 @@ const VestingSchedule = () => {
         <div className="procedureNumber">
           <img src={tick} alt="tick-icon" />
         </div>
-        <div className='vesting-schedule-text'>
-          <p className='vesting-percentage'>30%</p>
-          <p className="vesting-text"><span className='claimable-text'>Claimable at</span> 12/17/2021, 9:30:00pm</p>
+        <div className="vesting-schedule-text">
+          <p className="vesting-percentage">23.3%</p>
+          <p className="vesting-text">
+            <span className="claimable-text">Claimable at</span> <br />
+            12/17/2021, 9:30:00pm
+          </p>
         </div>
       </div>
 
@@ -33,19 +39,25 @@ const VestingSchedule = () => {
         <div className="procedureNumber">
           <img src={tick} alt="tick-icon" />
         </div>
-        <div className='vesting-schedule-text'>
-          <p className='vesting-percentage'>30%</p>
-          <p className="vesting-text"><span className='claimable-text'>Claimable at</span> 12/17/2021, 9:30:00pm</p>
+        <div className="vesting-schedule-text">
+          <p className="vesting-percentage">23.3%</p>
+          <p className="vesting-text">
+            <span className="claimable-text">Claimable at</span> <br />
+            12/17/2021, 9:30:00pm
+          </p>
         </div>
       </div>
 
       <div className="procedure vesting-procedure">
-      <div className="procedureNumber">
+        <div className="procedureNumber">
           <img src={tick} alt="tick-icon" />
         </div>
-        <div className='vesting-schedule-text'>
-          <p className='vesting-percentage'>30%</p>
-          <p className="vesting-text"><span className='claimable-text'>Claimable at</span> 12/17/2021, 9:30:00pm</p>
+        <div className="vesting-schedule-text">
+          <p className="vesting-percentage">23.4%</p>
+          <p className="vesting-text">
+            <span className="claimable-text">Claimable at</span> <br />
+            12/17/2021, 9:30:00pm
+          </p>
         </div>
       </div>
     </div>
