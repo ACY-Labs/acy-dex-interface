722a435416e0fad8bd605801285ad21fb3dc46d2
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-12-20 10:09:10 +0800

    feat: fetch transaction details for specific accounts in market

--- src/pages/Market/Data/txData.js
+++ src/pages/Market/Data/txData.js
@@ -25,7 +25,8 @@ async function parseGlobalTransaction(userList,library){
    let txList = [];
    let finalTxList = [];
 
-   for( const user of userList){
+  for (const user of userList) {
+     console.log(user)
      const userSwapTx = await getTransactionsByAccount(user,library,'SWAP');
      const _swaptx = userSwapTx.map((item) => {
        return {
@@ -72,6 +73,57 @@ export async function fetchGlobalTransaction(library){
     console.log(data.data);
     let globalTransactions = await parseGlobalTransaction(data.data,library);
     return globalTransactions;
+  }catch (e){
+    console.log('service not available yet',e);
+    return null;
+  }
+}
+
+async function parseAccountTransaction(user,library){
+  let txList = [];
+  let finalTxList = [];
+
+    const userSwapTx = await getTransactionsByAccount(user,library,'SWAP');
+    const _swaptx = userSwapTx.map((item) => {
+      return {
+       account: user,
+       coin1: item.inputTokenSymbol,
+       coin1Amount: item.inputTokenNum,
+       coin2: item.outTokenSymbol,
+       coin2Amount: item.outTokenNum,
+       time: item.transactionTime,
+       totalValue: item.totalToken,
+       transactionID: item.hash,
+       type: "Swap"
+      }
+    })
+    const userLiquidityTx = await getTransactionsByAccount(user,library, 'LIQUIDITY');
+
+    const _liquiditytx = userLiquidityTx.map((item) => {
+     return {
+      account: user,
+      coin1: item.token1Symbol,
+      coin1Amount: item.token1Number,
+      coin2: item.token2Symbol,
+      coin2Amount: item.token2Number,
+      time: item.transactionTime,
+      totalValue: item.totalToken,
+      transactionID: item.hash,
+      type: item.action
+     }
+   })
+
+  txList.push(..._swaptx);
+  txList.push(..._liquiditytx);
+
+  return sortTable(txList, "time", true);
+}
+
+
+export async function fetchAccountTransaction(account, library) {
+  try{
+    let accountTransactions = await parseAccountTransaction(account, library);
+    return accountTransactions;
   }catch (e){
     console.log('service not available yet',e);
     return null;
