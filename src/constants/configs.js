82e53124f5e33835562d616246bfae19ab565c76
Carlos <carlos.ramos@grupoimportia.com>
2021-12-27 14:34:39 +0800

    (wip) : added constant for API

--- src/constants/configs.js
+++ src/constants/configs.js
@@ -1,1 +1,8 @@
 export const scanUrlPrefix = "https://www.bscscan.com";
+//API FOR TESTING
+export const ACY_API = 'http://localhost:3001/api/';
+export const ACY_ROUTER = "0x4DCa8E42634abdE1925ebB7f82AC29Ea00d34bA2";
+// export const ACY_API = 'https://api.acy.finance/api/';
+
+
+export const DAYS_TRACKED_CHART = 20;
