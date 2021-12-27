94be5fdc5ad9b61948e2a17cc465c48bd69ff740
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-12-27 23:34:51 +0800

    feat(develop_launchpad): claim button in vesting schedule

--- src/pages/LaunchPad/VestingSchedule.js
+++ src/pages/LaunchPad/VestingSchedule.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './css/LaunchpadProject.css';
 import tick from '@/assets/icon-tick-white.svg';
 import moment from 'moment'
+import { useState } from "react";
 
 const VestingSchedule = ({ startDate }) => {
   let vestingStartDate = moment(startDate);
-  console.log(vestingStartDate)
   return (
     <div>
       <div className="procedure vesting-procedure">
@@ -14,7 +14,10 @@ const VestingSchedule = ({ startDate }) => {
           <img src={tick} alt="tick-icon" className="vesting-tick-icon"/>
         </div>
         <div className="vesting-schedule-text">
-          <p className="vesting-percentage">30%</p>
+          <div className='vesting-percentage-claim-container'>
+            <p className="vesting-percentage">30%</p>
+            <button className={vestingStartDate.isBefore() ? "claim-btn claim-btn-active" : "claim-btn claim-btn-inactive"} id="claim-btn-1">Claim</button>
+          </div>
           <p className="vesting-text">
             <span className="claimable-text">Claimable at</span>
             <br />
@@ -29,7 +32,10 @@ const VestingSchedule = ({ startDate }) => {
           <img src={tick} alt="tick-icon" className="vesting-tick-icon"/>
         </div>
         <div className="vesting-schedule-text">
-          <p className="vesting-percentage">23.3%</p>
+        <div className='vesting-percentage-claim-container'>
+            <p className="vesting-percentage">23.3%</p>
+            <button className={vestingStartDate.isBefore() ? "claim-btn claim-btn-active" : "claim-btn claim-btn-inactive"} id="claim-btn-1">Claim</button>
+          </div>
           <p className="vesting-text">
             <span className="claimable-text">Claimable at</span> <br />
             {vestingStartDate.add(1, "months").format("h:mm A D MMM YYYY")}
@@ -43,7 +49,10 @@ const VestingSchedule = ({ startDate }) => {
           <img src={tick} alt="tick-icon" className="vesting-tick-icon"/>
         </div>
         <div className="vesting-schedule-text">
-          <p className="vesting-percentage">23.3%</p>
+        <div className='vesting-percentage-claim-container'>
+            <p className="vesting-percentage">23.3%</p>
+            <button className={vestingStartDate.isBefore() ? "claim-btn claim-btn-active" : "claim-btn claim-btn-inactive"} id="claim-btn-1">Claim</button>
+          </div>
           <p className="vesting-text">
             <span className="claimable-text">Claimable at</span> <br />
             {vestingStartDate.add(2, "months").format("h:mm A D MMM YYYY")}
@@ -56,7 +65,10 @@ const VestingSchedule = ({ startDate }) => {
           <img src={tick} alt="tick-icon" className="vesting-tick-icon"/>
         </div>
         <div className="vesting-schedule-text">
-          <p className="vesting-percentage">23.4%</p>
+        <div className='vesting-percentage-claim-container'>
+            <p className="vesting-percentage">23.4%</p>
+            <button className={vestingStartDate.isBefore() ? "claim-btn claim-btn-active" : "claim-btn claim-btn-inactive"} id="claim-btn-1">Claim</button>
+          </div>
           <p className="vesting-text">
             <span className="claimable-text">Claimable at</span> <br />
             {vestingStartDate.add(3, "months").format("h:mm A D MMM YYYY")}
