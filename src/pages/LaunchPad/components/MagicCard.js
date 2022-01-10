24004d876b15419832c7fdcec5c926f831fc1d73
John Khor <john.khor97@gmail.com>
2022-01-11 00:42:59 +0800

    fix(develop_launchpad): added project and fix project token icon display

--- src/pages/LaunchPad/components/MagicCard.js
+++ src/pages/LaunchPad/components/MagicCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 import '../css/MagicCard.css';
 import ProjectsCard from './ProjectsCard.js';
 
-const MagicCard = ({ projectID, start, ddl, raise, sales, rate, title, isOngoing }) => {
+const MagicCard = ({ projectID, start, ddl, raise, sales, rate, title, projectTokenUrl, isOngoing }) => {
   return (
     <div className="magic-card-body">
       <div className="magic-card">
-        <ProjectsCard projectID={projectID} start={start} ddl={ddl} raise={raise} sales={sales} rate={rate} title={title} isOngoing={isOngoing} />
+        <ProjectsCard projectID={projectID} start={start} ddl={ddl} raise={raise} sales={sales} rate={rate} title={title} tokenLogoUrl={projectTokenUrl} isOngoing={isOngoing} />
       </div>
       <div className="magic-card-before" />
       <div className="magic-card-after" />
