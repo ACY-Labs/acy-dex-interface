facf6d6f04f92eab0311afd65c92f779658ede72
Carlos <carlos.ramos@grupoimportia.com>
2022-01-25 13:56:27 +0800

    (wip) : close position modal

--- src/components/AcyClosePositionModal/index.js
+++ src/components/AcyClosePositionModal/index.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useState, useRef } from 'react';
 import { connect } from 'umi';
-// import { getEstimated, signOrApprove, removeLiquidity } from '@/acy-dex-swap/core/removeLiquidity';
 import { Icon, Button, Input } from "antd";
+import Switch from '@material-ui/core/Switch';
 import styles from './index.less';
 import {
     AcyModal,
@@ -14,91 +14,185 @@ import { useConstantLoader } from '@/constants';
 import classNames from 'classnames';
 import { findTokenWithSymbol } from '@/utils/txData';
 import { getUserTokenBalance } from '@/acy-dex-swap/utils';
+import { formatAmount, USD_DECIMALS, mapPositionData, parseValue } from '@/utils/utils';
 
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {},
+  track: {},
+  switchBase: {
+    color: '#FFF',
+    '&$checked': {
+      color: '#eb5c20',
+    },
+    '&$checked + $track': {
+      backgroundColor: '#a34520',
+    },
+  },
+  checked: {},
+  thumb: {},
+});
 
 const AcyClosePositionModal = ({ Position, isModalVisible, onCancel, ...props }) => {
+  const classes = useStyles();
   const { account, chainId, library, farmSetting: { API_URL: apiUrlPrefix }, farmSetting: { INITIAL_ALLOWED_SLIPPAGE } } = useConstantLoader()
-  const [editToken, setEditToken] = useState({symbol : 'ACY'});
+  const [fromToken, setFromToken] = useState({symbol : 'ACY'});
   const [editIsLong, setEditIsLong] = useState(false);
-  const [editPositionInfo, setEditPositionInfo] = useState([]);
-  const [editTokenInputAmount, setEditTokenInputAmount] = useState('');
-  const [editTokenBalance, setEditTokenBalance] = useState();
-
+  const [positionInfo, setPositionInfo] = useState({});
+  const [newPositionInfo, setNewPositionInfo] = useState();
+  const [fromAmount, setFromAmount] = useState('');
+  const [fromAmountUSD, setFromAmountUSD] = useState(0);
+  const [maxFromAmount, setMaxFromAmount] = useState(0);
+  const [keepLeverage, setKeepLeverage] = useState(false);
+  const [buttonContent, setButtonContent] = useState('Enter Amount');
+  const [buttonIsEnabled, setButtonIsEnabled] = useState(false);
+  
   useEffect(() => {
 
     console.log("first use effect from acy edit", Position);
    if(Position){
-       setEditToken(findTokenWithSymbol('USDT'));
+       setFromToken(findTokenWithSymbol('USDT'));
        setEditIsLong(Position.isLong);
-
-       setEditPositionInfo([
-           {title : 'Size',
-            value : Position.totalSize.toFixed(2)},
-            {title : 'Collateral',
-            value : Position.collateral.toFixed(2)},
-            {title : 'Leverage',
-            value : `${Position.delta.toFixed(2)}x`},
-            {title : 'Mark Price',
-            value : Position.markPrice.toFixed(2)},
-            {title : 'Liq. Price',
-            value : Position.liqPrice.toFixed(2)}
-       ]);
-    setEditTokenInputAmount('');
+       setPositionInfo(mapPositionData(Position,'none'));
+       setMaxFromAmount(parseFloat(formatAmount(Position.size,USD_DECIMALS,2,null,false)));
    }   
   },[Position]);
 
-  useEffect( async () => {
-    console.log("updating balance for edit token", editToken);
-    setEditTokenBalance(await getUserTokenBalance(editToken, chainId, account, library).catch(e => console.log("Edit position update balance dispatch error",e)));
-  },[editToken])
+  useEffect(async ()=>{
+    if(Position){
+      const sizeDelta = parseValue(fromAmount,USD_DECIMALS);
+      setNewPositionInfo(mapPositionData(Position, 'Close', sizeDelta, keepLeverage));
+    }
+  },[fromAmount, keepLeverage])
+
+  const lockButton = (err)=> {
+    //err = 0 means normal enter ammount
+    //err = 1 means input amount exceeds maxFromAmount , 
+    //err = 2 means ....
+
+    switch(err){
+      case 0 :
+        setButtonContent('Enter Amount');
+        setButtonIsEnabled(false);
+        return 
+      case 1 :
+        setButtonContent('Max close amount exceeded');
+        setButtonIsEnabled(false);
+        return;
+    }
+    
+  }
+
+  const onInputChanged = (input, maxFromAmount) => {
+
+  
+    if(input == '') {
+      lockButton(0);
+      return
+    }
+  
+    if(input <= 0){
+      lockButton(0);
+      return 
+    }
+  
+    if(input > maxFromAmount) {
+      lockButton(1);
+      return;
+    }
+  
+    setButtonContent('Close');
+    setButtonIsEnabled(true);
+  
+  }
+
+  const resetParams = () => {
+    setFromAmount('');
+    lockButton(0);
+    setKeepLeverage(false);
+    setPositionInfo(mapPositionData(Position,'none'));
+  }
 
   const handleCancel = () => {
     onCancel();
-    //cancel and reset all parameters
+    resetParams();
   }
 
   return (
     <AcyModal backgroundColor="#0e0304" width={400} visible={isModalVisible} onCancel={handleCancel}>
-      <div className={styles.modalTitle} >Close {editIsLong ? 'Long' : 'Short'} {Position && Position.token.symbol}</div>
+      <div className={styles.modalTitle} >Close {editIsLong ? 'Long' : 'Short'} {Position && Position.indexToken.symbol}</div>
       <AcyCuarrencyCard
-              icon={editToken.symbol}
-              title={`Max ${Position && Position.totalSize.toFixed(2)}`}
-              logoURI={editToken.logoURI}
-              coin={editToken.symbol}
+              icon={fromToken.symbol}
+              title={`Max ${Position && formatAmount(Position.size, USD_DECIMALS, 2, null , true)}`}
+              logoURI={fromToken.logoURI}
+              coin={fromToken.symbol}
               yuan="566.228"
-              dollar={`${editTokenBalance}`}
-              token={editTokenInputAmount}
+              dollar={`${maxFromAmount}`}
+              token={fromAmount}
               showBalance={true}
-              onChoseToken={async () => {
-                // onClickCoin();
-                // setBefore(true);
-              }}
               onChangeToken={e => {
-                setEditTokenInputAmount(e);
+                setFromAmount(e);
+                onInputChanged(e, maxFromAmount);
               }}
               isLocked={false}
               library={library}
             />
+        <div className={styles.checkboxContainer}>
+        <span>Keep Leverage at {positionInfo.Leverage}x</span>
+        <Switch
+              name="checkboxP"  
+              size="small"  
+              id="stake-switch" 
+              className={styles.checkbox}
+              onChange={(e) => {
+                setKeepLeverage(e.target.checked);
+              }}
+              checked={keepLeverage}
+              classes={{
+                root: classes.root,
+                switchBase: classes.switchBase,
+                checked: classes.checked,
+                track: classes.track,
+                thumb: classes.thumb,
+            }} 
+        />
+        </div>
+        <hr className={styles.linediv}/>
         <AcyDescriptions>
-              {editPositionInfo.map((item)=>{
-                       return <div className={styles.infoSmallBox}>
-                               <div className={styles.infoBoxTitle}>{item.title}</div>
-                               <div className={styles.infoBoxValue}>
-                                   {item.value}
-                               </div>
-                           </div>
-                  })
+              {Object.keys(positionInfo).map( (key, index) => {
+                  return <div className={styles.infoSmallBox}>
+                    <div className={styles.infoBoxTitle}>{key}</div>
+                    { newPositionInfo && key in newPositionInfo ? 
+                        <div className={styles.infoBoxValue}>
+                            <div>
+                              <div className={styles.inlineInfoBlock}>
+                                $ 
+                                {positionInfo[key]}
+                                <svg className={styles.infoBoxTransitionArrow}>
+                                  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                                </svg>
+                              </div>
+                                {newPositionInfo[key]}
+                            </div>
+                        </div> 
+                      :
+                        <div className={styles.infoBoxValue}>
+                          $ 
+                         {positionInfo[key]}
+                        </div> }
+                  </div> } )
               }
         </AcyDescriptions>
         <div className={styles.buttonContainer}>
         <AcyButton
           variant="sucess"
-          disabled={true}
+          disabled={!buttonIsEnabled}
           onClick = {() => {
             console.log("PERFORM ACTION");
           }}
           >
-        Enter Amount
+        {buttonContent}
         </AcyButton>
         </div>
         
