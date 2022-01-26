176437f501af734fbc7a987f06d305eec932897d
Carlos <carlos.ramos@grupoimportia.com>
2022-01-26 11:50:38 +0800

    (wip) : added function to format amounts

--- src/pages/Perpetual/components/PositionsTable.js
+++ src/pages/Perpetual/components/PositionsTable.js
@@ -11,6 +11,7 @@ import AcyClosePositionModal from '@/components/AcyClosePositionModal';
 import AcyCreateOrderModal from '@/components/AcyCreadeOrderModal';
 import { isDesktop } from '@/pages/Market/Util';
 import {sortTableTime} from '../utils'
+import { formatAmount } from '@/utils/utils';
 
 const PositionsTable = props => {
   const [displayNumber, setDisplayNumber] = useState(5);
