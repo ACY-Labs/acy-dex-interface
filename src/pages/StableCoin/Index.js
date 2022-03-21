07003aba2c60ddec62d858acbcbe75ed7648a463
Carlos <carlos.ramos@grupoimportia.com>
2022-03-21 16:21:27 +0800

    (fix) : layout mistmatching for small screen

--- src/pages/StableCoin/Index.js
+++ src/pages/StableCoin/Index.js
@@ -1,41 +1,40 @@
-import PageHeaderWrapper from "@/components/PageHeaderWrapper";
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 import styles from './styles.less';
-import { connect } from "umi";
+import { connect } from 'umi';
 import Media from 'react-media';
-import { Banner } from "./components/banner";
-import { ExchangeTable } from "./components/exchangeTable"
+import { Banner } from './components/banner';
+import { ExchangeTable } from './components/exchangeTable';
 import { AcyCard } from '@/components/Acy';
 import SwapComponent from '@/components/SwapComponent';
-import { APYtable } from "./components/apytable"
-import { AccountBox } from "./components/accountBox"
+import { APYtable } from './components/apytable';
+import { AccountBox } from './components/accountBox';
 
 const icon = require('./aperture.svg');
 
-const StableCoin = (props) => {
+const StableCoin = props => {
+  return (
+    <PageHeaderWrapper>
+      <Banner />
+      
+      <div className={styles.dataBlock}>
+        <AccountBox />
+        <ExchangeTable />
+      </div>
 
-    return (
-        <PageHeaderWrapper>
-        <Banner></Banner>
-        <div className={styles.dataBlock}>
-            <AccountBox></AccountBox>
-            <ExchangeTable></ExchangeTable>
-        </div>
-        
-            <div className={styles.swapCard}>
-                <div className={`${styles.colItem} ${styles.swapComponent}`} >
-                    <AcyCard style={{ backgroundColor: '#0e0304', padding: '10px' }}>
-                        <div className={styles.trade}>
-                            <SwapComponent></SwapComponent>
-                        </div>
-                    </AcyCard>
-                </div>
+      <div className={styles.swapCard}>
+        <div className={`${styles.colItem} ${styles.swapComponent}`}>
+          <AcyCard style={{ backgroundColor: '#0e0304', padding: '10px' }}>
+            <div className={styles.trade}>
+              <SwapComponent />
             </div>
-
-        </PageHeaderWrapper>
-    )
-}
+          </AcyCard>
+        </div>
+      </div>
+    </PageHeaderWrapper>
+  );
+};
 
 export default connect(({ profile, loading }) => ({
-    profile,
-    loading: loading.effects['profile/fetchBasic'],
+  profile,
+  loading: loading.effects['profile/fetchBasic'],
 }))(StableCoin);
--- src/pages/StableCoin/components/accountBox.js
+++ src/pages/StableCoin/components/accountBox.js
@@ -1,4 +1,3 @@
-
 import styles from './accountBox.less';
 export const AccountBox = props => {
   //read props
