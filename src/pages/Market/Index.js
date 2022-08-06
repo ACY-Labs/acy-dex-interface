7886c1bf79a7b8ea1e739ec9ed62c1c4061faaa4
Joylim <joylimjiayin@gmail.com>
2022-08-06 18:20:14 +0800

    feat(develop_market): edit market tables

--- src/pages/Market/Index.js
+++ src/pages/Market/Index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ConnectWallet from './ConnectWallet';
 import { useConstantLoader } from '@/constants';
 import { useConnectWallet } from '@/components/ConnectWallet';
-import { PairsTable } from './TableComponent';
+import { PairsTable, LivePairsTable, TopVolumeTable, TrendingTable } from './TableComponent';
 // import { CurrencyTable } from './UtilComponent.js';
 // import { Icon } from 'antd';
 
@@ -11,7 +11,7 @@ import styles from './styles.less';
 const MarketIndex = props => {
 
   const { account, library, chainId } = useConstantLoader();
-  const [mode, setMode] = useState('pairs')
+  const [mode, setMode] = useState('Pairs')
 
   // connect to provider, listen for wallet to connect
   const connectWalletByLocalStorage = useConnectWallet();
@@ -25,11 +25,6 @@ const MarketIndex = props => {
     }
   }, []);
 
-  // .logoURI, .name
-  // .exchange, price, price_24h
-  // volume, swaps
-  // liquidity, fdv
-
   const test_pairs = [
     {
       name: 'USDT/WBNB',
@@ -37,6 +32,8 @@ const MarketIndex = props => {
       exchange: 'Pancakeswap v2',
       price: '$315.31',
       price_24h: '-0.56%',
+      price_7d: '-0.56%',
+      price_30d: '-0.56%',
       volume: '$31.66M',
       swaps: '70.15K',
       liquidity: '167.55M',
@@ -48,6 +45,8 @@ const MarketIndex = props => {
       exchange: 'Velodrome',
       price: '$1.9125',
       price_24h: '-2.76%',
+      price_7d: '-2.76%',
+      price_30d: '-2.76%',
       volume: '$18.89M',
       swaps: '69.30K',
       liquidity: '-',
@@ -59,10 +58,51 @@ const MarketIndex = props => {
       exchange: 'Pancakeswap v2',
       price: '$314.76',
       price_24h: '-0.77%',
+      price_7d: '-0.77%',
+      price_30d: '-0.77%',
       volume: '$31.50M',
       swaps: '55.37K',
       liquidity: '179.72M',
       fdv: '$1527.01B'
+    },
+  ]
+
+  const test_livepairs = [
+    {
+      name: 'USDC/PANKY',
+      logoURI: 'https://assets.coingecko.com/coins/images/325/large/Tether-logo.png?1598003707',
+      listed_since: '32s',
+      price: '-',
+      volume: '-',
+      initial_liquidity: '-',
+      total_liquidity: '-',
+      pool_amount: '-',
+      pool_variation: '0%',
+      pool_remaining: '-',
+    },
+    {
+      name: 'WETH/ROWA',
+      logoURI: 'https://assets.coingecko.com/coins/images/325/large/Tether-logo.png?1598003707',
+      listed_since: '2m 40s',
+      price: '$0.00002431',
+      volume: '-',
+      initial_liquidity: '2022-08-06',
+      total_liquidity: '$5310.16',
+      pool_amount: '1.55 ETH',
+      pool_variation: '0.06%',
+      pool_remaining: '1.6236 ETH',
+    },
+    {
+      name: 'WETH/OYA',
+      logoURI: 'https://assets.coingecko.com/coins/images/325/large/Tether-logo.png?1598003707',
+      listed_since: '10m 30s',
+      price: '$0.00004183',
+      volume: '-',
+      initial_liquidity: '2022-08-06',
+      total_liquidity: '$5914.87',
+      pool_amount: '1.5 ETH',
+      pool_variation: '15.13%',
+      pool_remaining: '1.72683 ETH',
     },
   ]
 
@@ -77,7 +117,7 @@ const MarketIndex = props => {
           <div className={styles.statsContainer}>
             <div className={styles.statstitle}>
               Live Pairs
-              <span className={styles.seeMore} onClick={() => { setMode('Live Pairs') }}>
+              <span className={styles.seeMore} onClick={() => { setMode('LivePairs') }}>
                 More >
               </span>
             </div>
@@ -108,7 +148,7 @@ const MarketIndex = props => {
           <div className={styles.statsContainer}>
             <div className={styles.statstitle}>
               Top Volume
-              <span className={styles.seeMore} onClick={() => { setMode('Top Volume') }}>
+              <span className={styles.seeMore} onClick={() => { setMode('TopVolume') }}>
                 More >
               </span>
             </div>
@@ -247,16 +287,16 @@ const MarketIndex = props => {
         <button
           type="button"
           className={styles.middleToggleButton}
-          style={{ backgroundColor: mode == 'Live Pairs' ? "#2e3032" : "transparent", color: mode == 'Live Pairs' ? "white" : "", border: '0.75px solid #333333' }}
-          onClick={() => { setMode('Live Pairs') }}
+          style={{ backgroundColor: mode == 'LivePairs' ? "#2e3032" : "transparent", color: mode == 'LivePairs' ? "white" : "", border: '0.75px solid #333333' }}
+          onClick={() => { setMode('LivePairs') }}
         >
           Live Pairs
         </button>
         <button
           type="button"
           className={styles.middleToggleButton}
-          style={{ backgroundColor: mode == 'Top Volume' ? "#2e3032" : "transparent", color: mode == 'Top Volume' ? "white" : "", border: '0.75px solid #333333' }}
-          onClick={() => { setMode('Top Volume') }}
+          style={{ backgroundColor: mode == 'TopVolume' ? "#2e3032" : "transparent", color: mode == 'TopVolume' ? "white" : "", border: '0.75px solid #333333' }}
+          onClick={() => { setMode('TopVolume') }}
         >
           Top Volume
         </button>
@@ -287,10 +327,11 @@ const MarketIndex = props => {
       </div>
 
       {mode == 'Pairs' && <PairsTable dataSource={test_pairs} />}
-
-
-
-
+      {mode == 'Winners' && <PairsTable dataSource={test_pairs} />}
+      {mode == 'Losers' && <PairsTable dataSource={test_pairs} />}
+      {mode == 'LivePairs' && <LivePairsTable dataSource={test_livepairs} />}
+      {mode == 'TopVolume' && <TopVolumeTable dataSource={test_pairs} />}
+      {mode == 'Trending' && <TrendingTable dataSource={test_pairs} />}
 
       {/* <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'baseline' }}>
         <h2>Cryptocurrencies</h2>
