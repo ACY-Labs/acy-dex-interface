d7b59742d28ae1ae326ad1db1caf30241720df8b
Joylim <joylimjiayin@gmail.com>
2022-08-11 21:18:54 +0800

    refactor(develop_market): change market page subtitles

--- src/pages/Market/Index.js
+++ src/pages/Market/Index.js
@@ -65,6 +65,84 @@ const MarketIndex = props => {
       liquidity: '179.72M',
       fdv: '$1527.01B'
     },
+    {
+      name: 'USDT/WBNB',
+      logoURI: 'https://assets.coingecko.com/coins/images/325/large/Tether-logo.png?1598003707',
+      exchange: 'Pancakeswap v2',
+      price: '$315.31',
+      price_24h: '-0.56%',
+      price_7d: '-0.56%',
+      price_30d: '-0.56%',
+      volume: '$31.66M',
+      swaps: '70.15K',
+      liquidity: '167.55M',
+      fdv: '$1223.66B'
+    },
+    {
+      name: 'USDC/OP',
+      logoURI: 'https://assets.coingecko.com/coins/images/6319/large/USD_Coin_icon.png?1547042389',
+      exchange: 'Velodrome',
+      price: '$1.9125',
+      price_24h: '-2.76%',
+      price_7d: '-2.76%',
+      price_30d: '-2.76%',
+      volume: '$18.89M',
+      swaps: '69.30K',
+      liquidity: '-',
+      fdv: '$8.20B'
+    },
+    {
+      name: 'BUSD/WBNB',
+      logoURI: 'https://assets.coingecko.com/coins/images/9576/large/BUSD.png?1568947766',
+      exchange: 'Pancakeswap v2',
+      price: '$314.76',
+      price_24h: '-0.77%',
+      price_7d: '-0.77%',
+      price_30d: '-0.77%',
+      volume: '$31.50M',
+      swaps: '55.37K',
+      liquidity: '179.72M',
+      fdv: '$1527.01B'
+    },
+    {
+      name: 'USDT/WBNB',
+      logoURI: 'https://assets.coingecko.com/coins/images/325/large/Tether-logo.png?1598003707',
+      exchange: 'Pancakeswap v2',
+      price: '$315.31',
+      price_24h: '-0.56%',
+      price_7d: '-0.56%',
+      price_30d: '-0.56%',
+      volume: '$31.66M',
+      swaps: '70.15K',
+      liquidity: '167.55M',
+      fdv: '$1223.66B'
+    },
+    {
+      name: 'USDC/OP',
+      logoURI: 'https://assets.coingecko.com/coins/images/6319/large/USD_Coin_icon.png?1547042389',
+      exchange: 'Velodrome',
+      price: '$1.9125',
+      price_24h: '-2.76%',
+      price_7d: '-2.76%',
+      price_30d: '-2.76%',
+      volume: '$18.89M',
+      swaps: '69.30K',
+      liquidity: '-',
+      fdv: '$8.20B'
+    },
+    {
+      name: 'BUSD/WBNB',
+      logoURI: 'https://assets.coingecko.com/coins/images/9576/large/BUSD.png?1568947766',
+      exchange: 'Pancakeswap v2',
+      price: '$314.76',
+      price_24h: '-0.77%',
+      price_7d: '-0.77%',
+      price_30d: '-0.77%',
+      volume: '$31.50M',
+      swaps: '55.37K',
+      liquidity: '179.72M',
+      fdv: '$1527.01B'
+    },
   ]
 
   const test_livepairs = [
@@ -110,18 +188,17 @@ const MarketIndex = props => {
     <div className={styles.marketRoot}>
       <ConnectWallet />
 
-      <div className={styles.chartGrid}>
-
-        {/* Live Pairs */}
+      {/* New Pairs */}
+      <div className={styles.topChart}>
         <div className={styles.chartCell}>
           <div className={styles.statsContainer}>
             <div className={styles.statstitle}>
-              Live Pairs
-              <span className={styles.seeMore} onClick={() => { setMode('LivePairs') }}>
+              New Pairs
+              <span className={styles.seeMore} onClick={() => { setMode('NewPairs') }}>
                 More >
               </span>
             </div>
-            <div className={styles.statsdivider} />
+            {/* <div className={styles.statsdivider} /> */}
 
             <div className={styles.statscontent}>
               <div className={styles.statsRow}>
@@ -138,10 +215,23 @@ const MarketIndex = props => {
                 <div className={styles.label}>3. WETH/Cocaine Inu</div>
                 <div className={styles.value}>XXX</div>
               </div>
+
+              <div className={styles.statsRow}>
+                <div className={styles.label}>4. WETH/Cocaine Inu</div>
+                <div className={styles.value}>XXX</div>
+              </div>
+
+              <div className={styles.statsRow}>
+                <div className={styles.label}>5. WETH/Cocaine Inu</div>
+                <div className={styles.value}>XXX</div>
+              </div>
             </div>
 
           </div>
         </div>
+      </div>
+
+      <div className={styles.chartGrid}>
 
         {/* Top Volume */}
         <div className={styles.chartCell}>
@@ -153,7 +243,7 @@ const MarketIndex = props => {
               </span>
             </div>
 
-            <div className={styles.statsdivider} />
+            {/* <div className={styles.statsdivider} /> */}
 
             <div className={styles.statscontent}>
               <div className={styles.statsRow}>
@@ -168,6 +258,16 @@ const MarketIndex = props => {
 
               <div className={styles.statsRow}>
                 <div className={styles.label}>3. USDT</div>
+                <div className={styles.value}>XXX</div>
+              </div>
+
+              <div className={styles.statsRow}>
+                <div className={styles.label}>4. USDT</div>
+                <div className={styles.value}>XXX</div>
+              </div>
+
+              <div className={styles.statsRow}>
+                <div className={styles.label}>5. USDT</div>
                 <div className={styles.value}>XXX</div>
               </div>
 
@@ -186,7 +286,7 @@ const MarketIndex = props => {
               </span>
             </div>
 
-            <div className={styles.statsdivider} />
+            {/* <div className={styles.statsdivider} /> */}
 
             <div className={styles.statscontent}>
               <div className={styles.statsRow}>
@@ -201,6 +301,16 @@ const MarketIndex = props => {
 
               <div className={styles.statsRow}>
                 <div className={styles.label}>3. DRX ARBI</div>
+                <div className={styles.value}>XXX</div>
+              </div>
+
+              <div className={styles.statsRow}>
+                <div className={styles.label}>4. DRX ARBI</div>
+                <div className={styles.value}>XXX</div>
+              </div>
+
+              <div className={styles.statsRow}>
+                <div className={styles.label}>5. DRX ARBI</div>
                 <div className={styles.value}>XXX</div>
               </div>
 
@@ -219,7 +329,7 @@ const MarketIndex = props => {
               </span>
             </div>
 
-            <div className={styles.statsdivider} />
+            {/* <div className={styles.statsdivider} /> */}
 
             <div className={styles.statscontent}>
               <div className={styles.statsRow}>
@@ -234,6 +344,16 @@ const MarketIndex = props => {
 
               <div className={styles.statsRow}>
                 <div className={styles.label}>3. WETH/XNOM</div>
+                <div className={styles.value}>XXX</div>
+              </div>
+
+              <div className={styles.statsRow}>
+                <div className={styles.label}>4. WETH/XNOM</div>
+                <div className={styles.value}>XXX</div>
+              </div>
+
+              <div className={styles.statsRow}>
+                <div className={styles.label}>5. WETH/XNOM</div>
                 <div className={styles.value}>XXX</div>
               </div>
             </div>
@@ -251,7 +371,7 @@ const MarketIndex = props => {
               </span>
             </div>
 
-            <div className={styles.statsdivider} />
+            {/* <div className={styles.statsdivider} /> */}
 
             <div className={styles.statscontent}>
               <div className={styles.statsRow}>
@@ -266,6 +386,16 @@ const MarketIndex = props => {
 
               <div className={styles.statsRow}>
                 <div className={styles.label}>3. WBNB/MINE</div>
+                <div className={styles.value}>XXX</div>
+              </div>
+
+              <div className={styles.statsRow}>
+                <div className={styles.label}>4. WBNB/MINE</div>
+                <div className={styles.value}>XXX</div>
+              </div>
+
+              <div className={styles.statsRow}>
+                <div className={styles.label}>5. WBNB/MINE</div>
                 <div className={styles.value}>XXX</div>
               </div>
             </div>
@@ -287,10 +417,10 @@ const MarketIndex = props => {
         <button
           type="button"
           className={styles.middleToggleButton}
-          style={{ backgroundColor: mode == 'LivePairs' ? "#2e3032" : "transparent", color: mode == 'LivePairs' ? "white" : "", border: '0.75px solid #333333' }}
-          onClick={() => { setMode('LivePairs') }}
+          style={{ backgroundColor: mode == 'NewPairs' ? "#2e3032" : "transparent", color: mode == 'LivePairs' ? "white" : "", border: '0.75px solid #333333' }}
+          onClick={() => { setMode('NewPairs') }}
         >
-          Live Pairs
+          New Pairs
         </button>
         <button
           type="button"
@@ -329,7 +459,7 @@ const MarketIndex = props => {
       {mode == 'Pairs' && <PairsTable dataSource={test_pairs} />}
       {mode == 'Winners' && <PairsTable dataSource={test_pairs} />}
       {mode == 'Losers' && <PairsTable dataSource={test_pairs} />}
-      {mode == 'LivePairs' && <LivePairsTable dataSource={test_livepairs} />}
+      {mode == 'NewPairs' && <LivePairsTable dataSource={test_livepairs} />}
       {mode == 'TopVolume' && <TopVolumeTable dataSource={test_pairs} />}
       {mode == 'Trending' && <TrendingTable dataSource={test_pairs} />}
 
